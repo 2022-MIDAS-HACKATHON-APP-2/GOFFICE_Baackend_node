4e6b7daec1ed7b42cb59c90d1a362d437579a3d1
2tle <iam@2tle.io>
2022-11-04 00:50:30 +0900

    commute

--- src/controllers/commute.ts
+++ src/controllers/commute.ts
@@ -2,4 +2,12 @@ import { Request, Response } from "express";
 import { CommuteEntity } from "../entities/Commute";
 import { getManager } from "typeorm";
 
-//export async function Create
+export async function goWork(req: Request, res: Response) {
+    const commuteRepository = getManager().getRepository(CommuteEntity);
+    // const currentTime = new Date();
+    // try {
+    //     const commute = await commuteRepository.findOne({
+    //         where: { user: req.decoded.id }
+    //     })
+    // }
+}
