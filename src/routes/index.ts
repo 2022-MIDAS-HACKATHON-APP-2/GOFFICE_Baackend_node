9249d379df12693dcf9f9eeeadc93e4604671fa1
yebin <yebin5127@gmail.com>
2022-11-04 01:57:58 +0900

    Fix index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,11 @@ import express from "express";
 import User from "./user";
 import Company from "./company";
 import Admin from "./admin";
-<<<<<<< HEAD
 import Post from "./post";
-=======
 import Commute from "./commute";
->>>>>>> main
+
 import { authMiddleware } from "../middlewares/auth";
+
 const router = express();
 
 router.use("/admin", authMiddleware, Admin);
