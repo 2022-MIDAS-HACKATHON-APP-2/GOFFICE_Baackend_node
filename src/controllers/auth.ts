50b4b226624dafb73fd0b331d3bb20ae4dd94b36
yebin <yebin5127@gmail.com>
2022-11-04 01:58:12 +0900

    Fix auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -69,15 +69,9 @@ export async function login(req: Request, res: Response) {
       if(user?.password == hashPassword) {
           const accessToken = jwt.sign(
               {
-<<<<<<< HEAD
-                  id: user.id,
-                  position: user.position,
-                  company_id: user.company_id
-=======
                   id: user?.id,
                   position : user.position,
                   company_id: user?.company_id
->>>>>>> main
               }, secretKey,
               {
                   expiresIn: "100h"
