7eab8d19f04b935bf82bfe3715bcc34f798370a5
yebin <yebin5127@gmail.com>
2022-11-04 01:10:05 +0900

    Create admin controller

---
+++ src/controllers/admin.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import { UserEntity } from "../entities/User";
+import { getManager } from "typeorm";
+import { CompanyEntity } from "../entities/Company";
+import { POSITION } from "../entities/common/Position";
+
+
+async function checkManager(position: string, res: Response) {
+    if(position !== POSITION.B) {
+        return res.status(403).json({
+            message: "관리자 권한 없음"
+        })
+    }
+};
+
+export async function viewMember(req: Request, res: Response) {
+    const memberRepository = getManager().getRepository(UserEntity);
+    const manager = (<any>req).decoded;
+
+    try{
+        checkManager(manager.position, res);
+        const members = await memberRepository.find(
+            { where : { company_id: manager.company_id, position: POSITION.A }}
+        );
+        const counting = await memberRepository.count(
+            { where : { company_id: manager.company_id, position: POSITION.A }}
+        );
+        res.status(200).json({
+            message: counting + "명",
+            members,
+        });
+    } catch(err) {
+        console.error(err);
+        res.status(404).json({
+            message: "찾을 수 없음"
+        });
+    }
+}
+export async function fixMember(req: Request, res: Response) {
+    const memberRepository = getManager().getRepository(UserEntity);
+    const manager = (<any>req).decoded;
+    const memberId = Number(req.params.user_id);
+    const { memberName, memberPosition } = req.body;
+
+    try{
+        
+        const member = await memberRepository.update({
+            id: memberId,
+            company_id: manager.company_id
+        }, {
+            name: memberName,
+            position: memberPosition
+        });
+
+        res.status(200).json({
+            message: "수정 성공",
+        });
+    } catch(err) {
+        console.error(err);
+        res.status(404).json({
+            message: "찾을 수 없음"
+        });
+    }
+}
