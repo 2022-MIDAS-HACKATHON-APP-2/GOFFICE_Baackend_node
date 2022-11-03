179a6ccabeb207a41927612d7cf0b26b821a4d40
yebin <yebin5127@gmail.com>
2022-11-04 03:48:14 +0900

    Update user route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import * as controller from "../controllers/auth";
+import { authMiddleware } from "../middlewares/auth";
 
 const router = express();
 
 router.post("/signup", controller.createUser);
 router.post("/login", controller.login);
+router.get("/:user_id", controller.showInfo);
 
 export default router;
