d18d7c3297899fcb3faabe7f22a4f29c7e7485bd
yebin <yebin5127@gmail.com>
2022-11-04 03:47:47 +0900

    Create restReq entity

---
+++ src/entities/restReq.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@decorators/di';
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn,
+    ManyToMany,
+    JoinTable,
+    PrimaryColumn,
+} from 'typeorm';
+import { CompanyEntity } from './Company';
+import { PostEntity } from './Post';
+import { UserEntity } from './User';
+
+@Entity({name : 'restReq'})
+export class RestReqEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    user_id: number;
+
+    @Column()
+    company_id: number;
+
+    @Column()
+    date: string;
+
+    @Column({ nullable: false })
+    reason: string;
+
+    @ManyToOne(() => UserEntity, (user) => user.comment)
+    @JoinColumn({ name: 'user_id' })
+    user: UserEntity;
+
+    @ManyToOne(() => CompanyEntity, (company) => company.restReq)
+    @JoinColumn({ name: 'company_id' })
+    company: CompanyEntity;
+
+    @CreateDateColumn()
+    createdAt: Date;
+}
