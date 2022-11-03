01ee4140da25fddff9064c53d0db36f20fb4acb9
2tle <iam@2tle.io>
2022-11-03 12:36:29 +0900

    Minor: add loader

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
-// import loaders from './loaders';
+import loaders from './loaders';
 
 async function bootstrap() {
     const app = express();
     dotenv.config();
-    //await loaders({ app });
+    await loaders({ app });
     app.listen(app.get('service_port'), () => {
         console.log(`Server is running port ${app.get('service_port')}`);
     });
