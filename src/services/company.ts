17f2e2f178624248faa606f2857864d3c7f60810
yebin <yebin5127@gmail.com>
2022-11-03 19:32:47 +0900

    Update company service

--- src/services/company.ts
+++ src/services/company.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from "@decorators/di";
 import { Repository } from "typeorm";
+import { WORK_TYPE } from "../entities/common/Worktype";
 import { CompanyEntity } from "../entities/Company";
 
 @Injectable()
@@ -9,9 +10,22 @@ export class CompanyService {
     ) {}
 
     async getCompanyId(companyName: string) {
-        const company = await this.companyEntity.createQueryBuilder("company").where("company.company_name =:name", {name: companyName}).getOne()
-        if(!company) return -1;
-        return company.id;
+        const company = await this.companyEntity.createQueryBuilder("company")
+            .select("company.id")
+            .where("company.company_name =:name", {name: companyName})
+            .getOne()
+        return company;
+    }
+
+    async AddCompany(companyName: string, workType: WORK_TYPE) {
+        return await this.companyEntity.createQueryBuilder("company")
+            .insert()
+            .into(CompanyEntity)
+            .values([
+                { company_name: companyName },
+                { work_type: workType}
+            ])
+            .execute();
     }
     
 }
