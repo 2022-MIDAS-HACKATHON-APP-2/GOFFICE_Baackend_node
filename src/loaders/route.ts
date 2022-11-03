2098fadeda15c8b53858b177834fa65e40fd0b55
yebin <yebin5127@gmail.com>
2022-11-03 19:32:21 +0900

    Update: route

--- src/loaders/route.ts
+++ src/loaders/route.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { attachControllers } from '@decorators/express';
 import { AuthController } from '../controllers/auth';
+import { CompanyController } from '../controllers/company';
 
 export default async (app: express.Application) => {
     attachControllers(app, [AuthController]);
+    attachControllers(app, [CompanyController]);
+       
 }
