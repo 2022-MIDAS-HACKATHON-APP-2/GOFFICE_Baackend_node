d2c9a06b191a9a2dd3e907eecd25937764036b4b
2tle <iam@2tle.io>
2022-11-03 23:58:00 +0900

    Minor: default test

---
+++ src/controllers/commute.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+import { CommuteEntity } from "../entities/Commute";
+import { getManager } from "typeorm";
+
+//export async function Create
---
+++ src/routes/commute.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import * as controller from "../controllers/commute";
+
+const router = express();
+
+
+export default router;
