6682c9314ae60b08332c48645bea0b0bf3c8f1fc
2tle <iam@2tle.io>
2022-11-03 18:46:52 +0900

    Fix: bug1

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import { AdminEntity } from "../entities/Admin";
 import { UserEntity } from "../entities/User";
 
-export const access = async( user: UserEntity, type: number) => {
+export const access = async( user: UserEntity | AdminEntity, type: number) => {
     const secretKey = (process.env.JWT_SECRET_KEY) ? process.env.JWT_SECRET_KEY : ""
     const token: string = await jwt.sign(
         {
