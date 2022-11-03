0e381f867ed84dcb05943bd69ebb43de82c467b4
yebin <yebin5127@gmail.com>
2022-11-03 23:39:57 +0900

    fix auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,12 +5,17 @@ export const authMiddleware = (
     req: Request,
     res: Response,
     next: NextFunction
-  ) => {
+) => {
     const token: any = req.headers["access-token"];
-    if (!token) return res.status(401).json({ message: "로그인 되어있지 않음" });
-    jwt.verify(token, req.app.get("jwt-secret"), (err: any, decoded: any) => {
-      if (err) return res.status(401).json({ message: "로그인 되어있지 않음" });
-      (<any>req).decoded = decoded;
-      next();
+    if(!token) return res.status(401).json({
+        message: "로그인 되어 있지 않음"
     });
-  };
+    
+    jwt.verify(token, req.app.get("jwt-secret"), (err: any, decoded: any) => {
+        if(err) return res.status(401).json({
+            message: "로그인 되어 있지 않음"
+        });
+        (<any>req).decoded = decoded;
+        next();
+    });
+}
