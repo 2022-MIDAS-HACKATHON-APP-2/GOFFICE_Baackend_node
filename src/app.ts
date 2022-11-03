67609ffeb7e63ef214a7acc1bc542acd0073d426
2tle <iam@2tle.io>
2022-11-03 12:34:49 +0900

    Feat: add

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
 /* Goffice Backend Application */
+
+import express from 'express';
+import dotenv from 'dotenv';
+// import loaders from './loaders';
+
+async function bootstrap() {
+    const app = express();
+    dotenv.config();
+    //await loaders({ app });
+    app.listen(app.get('service_port'), () => {
+        console.log(`Server is running port ${app.get('service_port')}`);
+    });
+}
+
+bootstrap();
