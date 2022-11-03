7efc9cc0e18082a317f62d92dfbed2e6862e589e
yebin <yebin5127@gmail.com>
2022-11-04 01:56:04 +0900

    Update admin controller

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -35,12 +35,13 @@ export async function viewMember(req: Request, res: Response) {
             message: "찾을 수 없음"
         });
     }
-}
+};
+
 export async function fixMember(req: Request, res: Response) {
     const memberRepository = getManager().getRepository(UserEntity);
     const manager = (<any>req).decoded;
-    const memberId = Number(req.params.user_id);
-    const { memberName, memberPosition } = req.body;
+    const memberId = Number(req.params.member_id);
+    const { memberName, memberPosition, department } = req.body;
 
     try{
         
@@ -49,11 +50,13 @@ export async function fixMember(req: Request, res: Response) {
             company_id: manager.company_id
         }, {
             name: memberName,
-            position: memberPosition
+            position: memberPosition,
+            department
         });
 
         res.status(200).json({
             message: "수정 성공",
+            member
         });
     } catch(err) {
         console.error(err);
@@ -61,4 +64,26 @@ export async function fixMember(req: Request, res: Response) {
             message: "찾을 수 없음"
         });
     }
-}
+};
+
+export async function viewOneMember(req: Request, res: Response) {
+    const memberRepository = getManager().getRepository(UserEntity);
+    const manager = (<any>req).decoded;
+    const memberId = Number(req.params.member_id);
+
+    try{
+        checkManager(manager.position, res);
+        const member = await memberRepository.findOne(
+            { where : { company_id: manager.company_id, position: POSITION.A, id: memberId }}
+        );
+        res.status(200).json({
+            member
+        });
+    } catch(err) {
+        console.error(err);
+        res.status(404).json({
+            message: "찾을 수 없음"
+        });
+    }
+};
+
