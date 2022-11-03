ee0d8a0e4aa6f94708d73941008c76eba9b73989
yebin <yebin5127@gmail.com>
2022-11-04 01:56:38 +0900

    Update index route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import User from "./user";
 import Company from "./company";
 import Admin from "./admin";
+import Post from "./post";
 import { authMiddleware } from "../middlewares/auth";
 const router = express();
 
 router.use("/admin", authMiddleware, Admin);
+router.use("/post", authMiddleware, Post)
 router.use("/user", User);
 router.use("/company", Company);
 
