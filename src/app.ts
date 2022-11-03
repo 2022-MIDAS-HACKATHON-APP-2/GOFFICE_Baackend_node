17280f277922d6d63faad24698a79267c811de5f
yebin <yebin5127@gmail.com>
2022-11-03 23:40:20 +0900

    Fix app

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,40 @@
-/* Goffice Backend Application */
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import express, { Request, Response, NextFunction } from "express";
+import router from "./routes/index";
+import cors from "cors";
 
-import express from 'express';
-import dotenv from 'dotenv';
-import loaders from './loaders';
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-async function bootstrap() {
-    const app = express();
-    dotenv.config();
-    await loaders({ app });
-    app.listen(process.env.PORT , () => {
-        console.log(`Server is running port ${process.env.PORT}`);
-    });
-}
+dotenv.config();
 
-bootstrap();
+const corsOptions = {
+    origin: '*',
+    method: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true,
+};
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set("jwt-secret", process.env.KEY);
+
+app.use(cors(corsOptions));
+app.use('/', router);
+
+
+app.listen(PORT, () => {
+    console.log(PORT, "번 포트에서 대기 중");
+
+    createConnection()
+        .then(() => {
+            console.log("데이터베이스 연결 성공");
+        })
+        .catch((err) => console.error(err));
+});
+
+export default app;
