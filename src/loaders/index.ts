2a382467dcffe8a7458b9bf08c2ccabad9cf2fb6
2tle <iam@2tle.io>
2022-11-03 13:17:49 +0900

    Minor: add loaders in loader index.js

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -3,9 +3,11 @@ import "reflect-metadata";
 import envLoader from './env';
 import databaseLoader from './db';
 import expressLoader from './express';
+import routeLoader from './route';
 
 export default async ({ app }: { app: Express }) => {
     await envLoader();
     await databaseLoader();
     await expressLoader(app);
+    await routeLoader(app);
 };
